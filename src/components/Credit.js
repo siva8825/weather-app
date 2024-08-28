710e13b3483d36d5cf9cb4342c7d9f6c9022b99a
siva8825 <163110790+siva8825@users.noreply.github.com>
2024-08-28 14:00:47 +0530

    Update Credit.js

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -5,9 +5,9 @@ export default function Credit() {
     <div class="credit">
     <p>
       Coded by
-      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
-        > Chaimae Lamirine</a
-      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+      <a href="https://siva-portfolio-new.netlify.app"
+        > Sivanesh Kumar</a
+      >, <a href="https://siva-weather-app.netlify.app">Open sourced on Github.</a> Hosted on Netlify
     </p>
   </div>
   );
